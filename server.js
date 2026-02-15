f04faea126f5c2f350cb45952a4641cf7b6d8bc1
asmaearradi1-maker <asmaearradi1@gmail.com>
2026-02-15 13:49:18 +0100

    Update server.js
    Changer le port de js  par le port noté dans supervision' monitoring' de l'application back sur Azure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middlewares
 app.use(cors()); // nécessaire si front hébergé ailleurs
